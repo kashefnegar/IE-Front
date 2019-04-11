193f5bef395c5be461237d7107937e712526173a
md131376st <mona131376st@gmail.com>
2019-04-11 13:43:58 +0430

    Add login form
    we used a standard html most of it

--- src/view/container/main/login/login.tsx
+++ src/view/container/main/login/login.tsx
@@ -9,20 +9,29 @@ import {BrowserRouter as Router,Route, Link} from 'react-router-dom'
 // var __html =require('src/view/template/static/base.html');
 import Header from 'src/view/container/header/header'
 import Footer from 'src/view/container/footer/footer'
+import Form_ from 'src/view/container/main/form/form'
+import {Button, FormCheck, FormControl, FormGroup, FormLabel, FormText} from 'react-bootstrap';
+import {Form} from 'react-bootstrap';
 // import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 // var template = { __html: __html };
 class Login extends Component<Props, State> {
     render() {
         return(
+
             <div>
                 {Header.call(this)}
                 <main>
-
+                    <form className={"login-form"} onSubmit={e => this.submitForm(e)}>
+                        <h4>نام کاربری</h4>
+                        <input type="text" placeholder="kashefnegar"/>
+                        {/**/}
+                        <h4>رمز عبور</h4>
+                        <input type="password" placeholder="******"/>
+                    </form>;
                 </main>
                 {Footer.call(this)}
             </div>);
-        // return( Header.call(this)
-        // Footer.call(this);)
+
     }
 
     submitForm(e: React.FormEvent<HTMLFormElement>): void {
