13fdae10f8e2bfa7d2446f4a5489ad41b98fab99
Negar <kashefnegar@gmail.com>
2019-04-11 15:48:32 +0430

    fixed top navigation bar

--- src/view/container/header/header.tsx
+++ src/view/container/header/header.tsx
@@ -2,24 +2,17 @@ import React, {Component} from 'react';
 import "./header.scss";
 const Header = () =>(
     <header >
-        <div className="header-right navbar-fixed-top navbar-default">
-            <div className={'sticky'}>
-            <a className="active" href="#account">حساب کاربری</a>
+        <div className={'sticky'}>
+        <div className={"header-right navbar-fixed-top navbar-default"}>
+            <div className={'icons'}>
+            <a className="active" href="#account" >حساب کاربری</a>
             <a className="active" href="#exit">خروج</a>
+            </div>
             <a href="#logo"> <img alt="logo" src ={ require('../../template/photo/Logo/logov2.png')}/> </a>
             </div>
         </div>
-
-        <div className="tifani bar">
-        </div>
     </header>
 );
-// class Header extends Component {
-//     render() {
-//         return (
-//
-//         );
-//     }
-// }
+
 
 export default Header;
