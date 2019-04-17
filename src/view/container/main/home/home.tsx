66282ae84a285121d4855b44b1444e87471e2c7c
md131376st <mona131376st@gmail.com>
2019-04-17 20:27:14 +0430

    Add json information from server

--- src/view/container/main/home/home.tsx
+++ src/view/container/main/home/home.tsx
@@ -1,14 +1,60 @@
 import React, {Component} from 'react';
+// import ReactJson from "react-json-view";
+import axios from 'axios'
 import Header from "src/view/container/header/header";
 import Footer from "src/view/container/footer/footer";
 import "./home.scss";
 import {Button, ButtonGroup, Col, Container, Form, FormControl, InputGroup, Row, ToggleButton} from "react-bootstrap";
-
+import {any, object} from "prop-types";
+import * as ts from "typescript/lib/tsserverlibrary";
+// import Project = ts.server.Project;
+// import {ServerResponse} from "http";
+// var json : any;
 var discription :string = 'این سایت بهترین کار را برای شما پیدا می کند';
-const data =[{"name":"test1"},{"name":"test2"}];
-const listItems = data.map((d) => <li key={d.name}>{d.name}</li>);
-class Home extends Component {
+// const data =[{"name":"test1"},{"name":"test2"}];
+// const listItems = data.map((d) => <li key={d.name}>{d.name}</li>);
+class Home extends Component<Props,State>{
+    constructor(parameters: { Props: Props, State: State }) {
+        let {Props, State} = parameters;
+        super(Props, State);
+        this.state= {
+            data:[]
+        };
+        // this.handleClick = this.handleClick.bind(this)
+    }
+    componentDidMount(): void
+     {
+        axios.get('http://localhost:8080/projects')
+            .then(response =>{
+
+                // transformResponse: (r: ServerResponse) => r.data
+                // console.log(response.data.Projects)
+                this.setState({
+                data:response.data.Projects
+                }
+            )
+            },error=>{
+                console.log('server errorrrr')
+            });
+    }
+            // .then(response => {
+            //     this.setState(
+            //         {
+            //             data:response.data})
+            // }
+            //     // const data = resp.data.data.children.map(obj => obj.data);
+            //     // this.setState({data:response.data})
+            // }
+
     render() {
+        // console.log("responseeeee");
+        console.log(this.state.data);
+        // @ts-ignore
+        // @ts-ignore
+        // @ts-ignore
+        // @ts-ignore
+        // @ts-ignore
+        // @ts-ignore
         return (
 
             <div>
@@ -47,8 +93,33 @@ class Home extends Component {
 
                     </div>
 
+                    <p>
+
+                        {this.state.data.map( (  Projects: Projects) =>(
 
 
+                            <li key={Projects.id}>
+                                    {
+                                        Projects.title
+                                    }
+                                    {
+                                        Projects.description
+                                    }
+                                    {Projects.budget}
+                                    {Projects.needskil.map((skill:skil)=>(
+                                        skill.name
+                                    ))}
+                                </li>
+                            ))
+                        }
+
+
+
+
+
+
+
+                    </p>
 
                 </main>
 
@@ -59,4 +130,32 @@ class Home extends Component {
     }
 }
 
-export default Home;
+interface Props {
+    // yourname : string;
+
+}
+interface State {
+    data: []
+
+}
+interface ServerResponse {
+    // data: Projects
+}
+
+interface Projects {
+    needskil:[]
+    userREQ:[]
+    description:string
+    id:string
+    title:string
+    deadline:number
+    budget:number
+
+
+}
+interface skil {
+    name:string
+    point:number
+}
+
+    export default Home;
