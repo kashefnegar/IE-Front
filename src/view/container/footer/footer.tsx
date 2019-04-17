79ae825acc427e4f3ddc21c81b179833ac3e5bd5
Negar <kashefnegar@gmail.com>
2019-04-17 23:38:17 +0430

    fixed footer text in the center - colors corrected

--- src/view/container/footer/footer.tsx
+++ src/view/container/footer/footer.tsx
@@ -2,15 +2,12 @@ import React, {Component} from 'react';
 import { Nav, Navbar} from 'react-bootstrap';
 import "./footer.scss"
 const  Footer = () =>(
-    <footer>
-        <Navbar bg="dark">
-            <Navbar.Brand>
-                <p  className="text-center mt-4 mb-4">
-                تمامی حقوق متعلق به جاب اونجا می باشد©
-                </p>
-            </Navbar.Brand>
-        </Navbar>
-    </footer>
+
+    <div className="footer-copyright py-1 text-center">
+        <a id="footer-link-react-copyright">
+            تمامی حقوق متعلق به جاب اونجا می باشد©
+        </a>
+    </div>
 );
 
 export default Footer;
