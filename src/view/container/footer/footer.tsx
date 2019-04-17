a532bd909c97e2bd660c72e2fcc0cdf79c98c34e
Negar <kashefnegar@gmail.com>
2019-04-18 00:21:16 +0430

    fixed footer at the end of page

--- src/view/container/footer/footer.tsx
+++ src/view/container/footer/footer.tsx
@@ -2,12 +2,13 @@ import React, {Component} from 'react';
 import { Nav, Navbar} from 'react-bootstrap';
 import "./footer.scss"
 const  Footer = () =>(
-
+    <footer>
     <div className="footer-copyright pyt-1 text-center">
         <a >
             تمامی حقوق متعلق به جاب اونجا می باشد©
         </a>
     </div>
+    </footer>
 );
 
 export default Footer;
