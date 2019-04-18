6e8af28f5a2bfe608be8005a6563de64972e7ff1
md131376st <mona131376st@gmail.com>
2019-04-18 14:10:28 +0430

    Adding user profile basic

---
+++ src/view/container/main/User_profile/user_profile.tsx
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import Header from "src/view/container/header/header";
+import Tifanibar from "src/view/container/header/tifanibar";
+import {Col, Row} from "react-bootstrap";
+import Footer from "src/view/container/footer/footer";
+import axios from "axios";
+
+
+class UserProfile extends Component <Props,State>{
+    constructor(parameters: { Props: Props, State: State }) {
+        let {Props, State} = parameters;
+        super(Props, State);
+        this.state = {
+            data: []
+        };
+    }
+    componentDidMount(): void{
+        // @ts-ignore
+        const { userid } = this.props.match.params;
+        axios.get("http://localhost:8080/user/"+userid)
+            .then(response=>{
+                this.setState({
+                        data:response.data
+                    }
+                )
+            }, error=>{
+                console.log('server errror')
+            })
+
+
+    }
+
+    render() {
+        console.log(this.state.data)
+        return (
+            <div>
+                {Header.call(this)}
+                {Tifanibar.call(this,false)}
+                <main>
+
+                </main>
+
+                {Footer.call(this)}
+
+            </div>
+        );
+    }
+}
+
+
+interface Props {
+    // yourname : string;
+
+}
+interface State {
+    data: []
+
+}
+export default UserProfile;
