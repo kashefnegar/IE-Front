812c05c56c7f13935fbd9528c567cb686a34c395
Negar <kashefnegar@gmail.com>
2019-05-17 22:23:48 +0430

    Send Login Info

--- src/view/container/main/login/login.tsx
+++ src/view/container/main/login/login.tsx
@@ -11,31 +11,66 @@ import Header from 'src/view/container/header/header'
 import Footer from 'src/view/container/footer/footer'
 import Tifanibar from 'src/view/container/header/tifanibar'
 import {Button, Form} from "react-bootstrap";
+import axios from "axios";
+const config = { headers: {'Content-Type': 'application/json'} };
 // import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 // var template = { __html: __html };
-class Login extends Component<Props, State> {
+class Login extends Component <props, state> {
+
+    constructor(props: props) {
+        super(props)
+        this.state = {
+            inputValue:'',
+            username: '',
+            password: '',
+        };
+    }
+    handleUsername(e){
+        this.setState({username: e.target.value});
+    }
+    handlePassword(e){
+        this.setState({password: e.target.value});
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        var user = {
+            username:this.state.username,
+            password:this.state.password,
+        };
+
+        axios.post(`http://localhost:8080/login`, { user } ,config)
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+            })
+    }
     render() {
 
         var div = (
             <div>
                 {Header.call(this)}
                 {Tifanibar.call(this,true)}
-                <main>
-                    <Form>
+                <main  >
+                    <Form action={'/'}>
 
                         <Form.Group controlId="formBasicEmail">
                             <Form.Label>نام کاربری</Form.Label>
-                            <Form.Control type="email" placeholder="kashefnegar"/>
+                            <Form.Control type="email" placeholder="kashefnegar"
+                                          onChange={(event) => this.handleUsername(event)}/>
                         </Form.Group>
 
                         <Form.Group controlId="formBasicPassword">
                             <Form.Label>رمز عبور</Form.Label>
-                            <Form.Control type="password" placeholder="******"/>
+                            <Form.Control type="password" placeholder="******"
+                                          onChange={(event) => this.handlePassword(event)}
+                            />
                         </Form.Group>
                         <Form.Group>
                             <span dir="rtl"><a href="/signup">  ثبت نام</a></span>
                             <Button variant="primary" type="submit">
-                                <a href="/">ورود</a>
+                                ورود
                             </Button>
 
                         </Form.Group>
@@ -59,12 +94,15 @@ class Login extends Component<Props, State> {
     }
 
 }
-interface Props {
+interface props {
     pnClick(inputValue: string): void;
 }
-interface State {
+interface state {
     inputValue: string;
+    username:string
+    password:string
 }
+
 
 // @ts-ignore
 export default Login;
